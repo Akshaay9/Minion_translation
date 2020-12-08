afe540459f68f2da4d8c8e23317438d2e6b16b09
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2020-12-08 18:12:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const translate = document.getElementById("translate")
+const translatedResult = document.getElementById("translated")
+const btn = document.getElementById("btn")
+var translatedValue = translate.value
+
+
+
+// API
+translatedResult.textContent = ""
+
+const translateApi =() => {
+
+
+    const url = `https://api.funtranslations.com/translate/vulcan.json?text=${translate.value}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(contents => {
+            var translatedData = contents.contents.translated
+            translatedResult.innerText = translatedData
+            translate.value = ""
+
+        })
+
+
+
+
+
+
+}
+
+btn.addEventListener("click", () => {
+    const translatedData = translateApi()
+
+})
+
+
+
